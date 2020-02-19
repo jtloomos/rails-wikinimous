4431da98351133a81f103408e1fed7108de065b9
James Loomos <jtloomos@gmail.com>
2020-02-19 09:13:33 -0300

    Made efforts on exercise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  article_title = Faker::Book.title
+  article_content = Faker::Movie.quote
+  Article.create(title: article_title, content: article_content)
+end
